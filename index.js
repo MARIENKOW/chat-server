579b015dd629f27b1b850f448babfb8ca2a0a41f
Denis Marienkow <den.marienkow@gmail.com>
2023-08-28 03:27:38 +0300

    webSocket middleware,some emits and events

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import * as dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import http from 'http';
 import { Server } from 'socket.io'
-import { disconnect } from "process";
 
 dotenv.config()
 
@@ -22,19 +21,59 @@ app.use('/', router);
 
 const web = http.Server(app);
 
-const socket = new Server(web, {
+const io = new Server(web, {
    cors: {
       origin: process.env.CLIENT_URL,
       credentials: true
    }
 })
+io.use((socket, next) => {
+   const { username, id } = socket.handshake.auth;
+   if (!username || !id) return next(new Error("invalid request"));
+   socket.username = username;
+   socket.id = id;
+   next();
+});
+io.on('connection', (sock) => {
+   sock.on("private message", ({ message, to }) => {
+      sock.to(to).emit("private message", {
+         message,
+         from: sock.id,
+      });
+   });
 
-socket.on('connection', (sock) => {
-   sock.on('message',(data)=>{
-      socket.emit('response',data)
-   })
+   //! ///---ONLIN/Offline---\\\ !\\
+
+   const users = [];
+   for (let arr of io.of("/").sockets) {
+      users.push({
+         id: arr[0],
+         username: arr[1].username,
+      });
+   }
+
+   sock.broadcast.emit(`users`, users); //отправить всем когда подключаешься
+   sock.emit(`users`, users); //отправить только себе когда подключвешься    
    sock.on('disconnect', () => {
-      console.log(`${sock.id} disconnect`);
+      const users = [];
+      for (let arr of io.of("/").sockets) {
+         users.push({
+            id: arr[0],
+            username: arr[1].username,
+         });
+      }
+      sock.broadcast.emit(`users`, users);
+   })
+   sock.on('findOnlineUsers',(users)=>{
+      const sockUsers = [...io.of("/").sockets]
+      const onlineUsers = users.map((el)=>{
+         el.online = false;
+         if(sockUsers.find((elem)=>elem[0]===el.id)){
+            el.online = true;
+         }
+         return el;
+      })
+      sock.emit('findOnlineUsers',onlineUsers)
    })
 })
 
